901746cb165b96258768c62c635982a105bbd475
Bibhash7 <tranceispermanent@gmail.com>
2024-01-18 03:54:45 +0530

    Initial Push

---
+++ PDFCropper.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfWriter,PdfReader
+
+def cropper(start,end,file):
+    if(start !=  0):
+        start -=1
+    inputPdf = PdfReader(open(file,"rb"))
+    if(start > len(inputPdf.pages) or end > len(inputPdf.pages)):
+        return len(inputPdf.pages)
+    outPdf = PdfWriter()
+    with open(file.split(".")[0]+"cropped"+".pdf","wb") as ostream:
+        for page in range(start, end):
+          outPdf.add_page(inputPdf.pages[page])
+          outPdf.write(ostream)
+    return 0
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import *
+
+import PDFCropper
+
+app = Flask(__name__)
+
+@app.route("/")
+def upload():
+    return render_template("file_upload.html")
+
+@app.route("/success",methods=["POST"])
+def success():
+    success.start_page=int(request.form['start'])
+    success.end_page=int(request.form['end'])
+    f=request.files['file']
+    success.file_name=f.filename
+    f.save(success.file_name)
+    return render_template("success.html",start=success.start_page,
+                           end=success.end_page,name=success.file_name)
+
+@app.route("/convert")
+def cropper():
+    flag = PDFCropper.cropper(success.start_page,success.end_page,success.file_name)
+    if(flag):
+        return render_template("failure.html",size = flag)
+    else:
+        return render_template("download.html")
+
+
+
+@app.route("/download")
+def download():
+    filename=success.file_name.split(".")[0]+"cropped.pdf"
+    return send_file(filename,as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
